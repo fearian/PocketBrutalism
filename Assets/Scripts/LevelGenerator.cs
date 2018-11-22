34b58c9ec59fe499020770e40984b9170749eddc
fearian <45230169+fearian@users.noreply.github.com>
2018-11-22 07:42:13 +0000

    lesson 02 creating grid Element arrays and project setup

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public int width = 5;
+	public int height = 10;
+	public gridElement gridElement;
+	public gridElement[] gridElements;
+
+	// Use this for initialization
+	void Start () {
+		
+		gridElements = new gridElement[width * width * height];
+
+		for(int y = 0; y < height; y++)
+		{
+			for(int x = 0; x < width; x++)
+			{
+				for (int z = 0; z < width; z++)
+				{
+					gridElement girdElementInstance = Instantiate(gridElement, new Vector3(x,y,z), Quaternion.identity, this.transform);
+					girdElementInstance.name = "GridElement_" + x + "_" + y + "_" + z;
+					gridElements[x+width*(z+width*y)] = girdElementInstance;
+				}
+			}
+		}	
+	}
+	
+}
